146d62223d9066fc8a8a8820540c79fe452402a4
Ihor Tarkhan <ihor.tarkhan.working@gmail.com>
2021-12-26 12:52:54 +0200

    enum v2

--- src/main/java/org/example/myhashmap/TestI.java
+++ src/main/java/org/example/myhashmap/TestI.java
@@ -10,6 +10,24 @@ public enum TestI {
     this.i = i;
   }
 
+  public char getShort2() {
+    char result;
+    switch (this.name()) {
+      case "AAA":
+        result = 'a';
+        break;
+      case "BBB":
+        result = 'b';
+        break;
+      case "CCC":
+        result = 'c';
+        break;
+      default:
+        result = '0';
+    }
+    return result;
+  }
+
   public char getShort() {
     return i;
   }
